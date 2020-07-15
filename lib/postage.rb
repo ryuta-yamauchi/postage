5d32312b7713c0e592f4351b2828c0444d3c7b15
ryuta.yamauchi <ryuta.yamauchi@gaiax.com>
2020-07-15 18:53:49 +0900

    first commit

---
+++ lib/postage.rb
@@ -0,0 +1,10 @@
+#require "postage/kuronekoyamato"
+require "postage/version"
+
+module Postage
+  class Error < StandardError; end
+
+  #class Kuronekoyamato
+  #  include KuronekoyamatoMethod 
+  #end
+end
